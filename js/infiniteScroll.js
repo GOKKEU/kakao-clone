13981e00e21f73f08974618cd55648b21f91a5df
GOKKEU <gwiy218@naver.com>
2020-03-14 03:37:35 +0900

    web캘린더애니메이션 완료
    2020.03.14 모바일은X (500Width의 웹 화면을 기준으로 애니메이션 적용/모바일은 이후  개발일정에 추가-남은과제는  이전해/이후해의 달력 생성

--- js/infiniteScroll.js
+++ js/infiniteScroll.js
@@ -1,35 +1,44 @@
 function myFunction() {
+  var touch = e.originalEvent.touches;
+
   document.querySelector(".boxwrap").style.background = "pink";
+  //box에
 }
 
 $(document).ready(function() {
+  printContainer(currentMonth - 1);
   printContainer(currentMonth);
   printContainer(currentMonth + 1);
-  document.querySelector(".boxwrap").addEventListener("touchmove", myFunction);
 
-  console.log("fist");
+  document.querySelector(".boxwrap").addEventListener("touchmove", myFunction);
+  const center = document.querySelector(".boxwrap").offsetWidth;
+  window.scrollTo(center, 0);
+  console.log("first" + center);
 });
 
 //마우스 휠 이벤시 시!
 $(function() {
   $("html, body").on("mousewheel DOMMouseScroll", function(e) {
+    const boxContainer = document.querySelector(".boxwrap");
     var wheelDelta = e.originalEvent.wheelDelta;
+    const viewWidth = boxContainer.offsetWidth;
 
+    console.log(viewWidth);
     if (wheelDelta > 0) {
       console.log("up");
       $(this).scrollLeft(-wheelDelta + $(this).scrollLeft());
-
+      window.scrollTo(viewWidth, 0);
       /*애니메이션*/
       $(this)
         .stop()
         .animate(
           {
-            scrollLeft: -20 + "px" //스크롤시 왼쪽에서 부터 0px 위치로 스크롤이 돌아온다!
+            scrollLeft: (viewWidth / 3) * 2 + "px" //스크롤시 왼쪽에서 부터 0px 위치로 스크롤이 돌아온다!
           },
           {
-            duration: 1000,
+            duration: 1200,
             complete: function() {
-              const boxContainer = document.querySelector(".boxwrap");
+              window.scrollTo(viewWidth, 0);
 
               const firstId = boxContainer.firstElementChild.id;
 
@@ -40,6 +49,8 @@ $(function() {
               if (firstNumber > 1) {
                 /*타이틀 수정*/
                 document.querySelector(".js-month").innerHTML = firstNumber - 1;
+                // boxContainer.lastElementChild.remove();
+
                 /*모든 자시 노드를 삭제*/
                 while (boxContainer.hasChildNodes()) {
                   boxContainer.removeChild(boxContainer.firstElementChild);
@@ -47,6 +58,7 @@ $(function() {
                 /* 자시 노드를 추가*/
                 printContainer(firstNumber - 1);
                 printContainer(firstNumber);
+                printContainer(firstNumber + 1);
               }
             }
           }
@@ -55,17 +67,18 @@ $(function() {
       console.log("down");
       /*마우스 휠로 좌우스크롤 이동*/
       $(this).scrollLeft(-wheelDelta + $(this).scrollLeft());
-
+      window.scrollTo(viewWidth, 0);
       /*애니메이션*/
       $(this)
         .stop()
         .animate(
           {
-            scrollLeft: 20 + "px" //스크롤시 왼쪽에서 부터 0px 위치로 스크롤이 돌아온다!
+            scrollLeft: "+=" + viewWidth / 3 + "px" //스크롤시 왼쪽에서 부터 0px 위치갔다가 스크롤이 돌아온다!
           },
           {
             duration: 1000,
             complete: function() {
+              window.scrollTo(viewWidth, 0);
               const boxContainer = document.querySelector(".boxwrap");
 
               const lastId = boxContainer.lastElementChild.id;
@@ -78,7 +91,15 @@ $(function() {
                 /*타이틀 수정*/
                 document.querySelector(".js-month").innerHTML = lastId;
 
-                boxContainer.lastElementChild.previousSibling.remove();
+                //boxContainer.lastElementChild.previousSibling.remove();
+                //printContainer(lastNumber + 1);
+                /*모든 자시 노드를 삭제*/
+                while (boxContainer.hasChildNodes()) {
+                  boxContainer.removeChild(boxContainer.firstElementChild);
+                }
+                /* 자시 노드를 추가*/
+                printContainer(lastNumber - 1);
+                printContainer(lastNumber);
                 printContainer(lastNumber + 1);
               }
             }
